8e90869247e62674b485ef0b2b33ce028fd51473
Paul Gumeniuk <g.pasha@email.ua>
2020-04-17 12:23:14 +0300

    Update

--- routes/index.js
+++ routes/index.js
@@ -5,28 +5,21 @@ var con = require("../demo_db_connection");
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-<<<<<<< HEAD
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Home' });
-=======
-
-  res.render('index', { title: 'Express' });
->>>>>>> fca66e36d94c9542c5a6cfe41c924122573e10b7
 });
 
-router.post('/form', function(req, res) {
+router.post('/form', function (req, res) {
   var data = req.body;
-  con.connect(function(err) {
+  con.connect(function (err) {
     if (err) throw err;
     con.query(`insert into sys.messages (username, surname, subject, email, message) 
                     values ('${data.username}', '${data.surname}', '${data.subject}', '${data.email}', '${data.message}')`);
   });
-
- res.redirect('/');
+  res.redirect('/');
 });
 
-router.get('*', function(req, res, next) {
-
+router.get('*', function (req, res, next) {
   res.send('not found', 404);
 });
 module.exports = router;
